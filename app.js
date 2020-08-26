075c67751d8219134d353b3d3e4266538119076c
Mathilde Congiu <mathilde.congiu@hotmail.com>
2020-08-26 23:11:24 +0200

    CSS and JavaScript corrected. Exercice over.

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-document.getElementById('pokemon')
+let count = 1;
+
+
+function render() {
+    let pokemon = document.getElementById('pokemon')
+pokemon.innerHTML = `<img class= "poke" src="https://tinyurl.com/ironhack-pokemons/${count}.svg">`
+
+}
+
+let prev = document.getElementById("prev-button");
+
+
+prev.onclick = function() {
+    if (count >1) {
+        count --;
+        render()
+    }
+}
+
+let next = document.getElementById("next-button");
+
+next.onclick = function() {
+    if (count < 650) {
+        count ++;
+        render()
+    }
+}
+
+render()
